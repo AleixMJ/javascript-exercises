9fb915e3975aaf213dbe532a7fcc98aa98339c66
AleixMJ <aleixtechIT@gmail.com>
2026-01-20 22:04:34 +0100

    Completed exercise 6 TOP

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
+
+let solution =""
+
+if (num == 0) {
+    return "";
+}
+
+else if (num > 0) {
+    for (let i = 0; i < num; i++) {
+        solution = solution + text;
+    }
+    return solution;
+}
+else {
+    return "ERROR";
+}
 
 };
 
+console.log(repeatString("hola",3))
 // Do not edit below this line
 module.exports = repeatString;
