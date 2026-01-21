403478e852d214b51ac553a3413181d57be55216
AleixMJ <aleixtechIT@gmail.com>
2026-01-21 10:22:24 +0100

    Completed exercise 7 TOP

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let solution = "";
+    for (let i = text.length -1; i >= 0 ; i--) {
+        solution = solution + text[i];
+    }
+    return solution
 
 };
+
+console.log(reverseString("hola"));
 
 // Do not edit below this line
 module.exports = reverseString;
