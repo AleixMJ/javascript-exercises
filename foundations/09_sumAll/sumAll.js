cfdcd2b54bfdc5f2f8bb38e5b443459445a305f3
AleixMJ <aleixtechIT@gmail.com>
2026-01-31 18:28:45 +0100

    completed exercise 9 TOP

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a , b) {
+    
+    if (a < 0 || b < 0 || Number.isInteger(a) === false || Number.isInteger(b) === false ) {
+        return "ERROR";
+    }
+    
+    let start = 0;
+    let end = 0;
+
+    if (a < b) {
+        start = a;
+        end = b;
+    }
+    else {
+        start = b;
+        end = a;
+    }
+    console.log(start);
+    console.log(end);
+    let total = 0;
+    while (start < end) {
+        total += start;
+        start++;
+    }
+    return total + end;
 
 };
 
