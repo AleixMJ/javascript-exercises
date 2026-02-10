804502c1e74e7a23335d0af172f6954bbb1d3d5a
AleixMJ <aleixtechIT@gmail.com>
2026-02-10 11:16:58 +0100

    added palindrome function and completed exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const textFiltered = text.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
+    let i = 0;
+    while (i < textFiltered.length -1) {
+        if (textFiltered[i] != textFiltered.at(-(i+1))) {
+            return false
+        }
+        else {
+            i++;
+        }
+    }
+    return true;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
