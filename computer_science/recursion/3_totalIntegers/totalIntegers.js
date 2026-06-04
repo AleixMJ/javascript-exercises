c03cf16fe5b371358fb176aa7f5eb52d89786aac
AleixMJ <aleixtechIT@gmail.com>
2026-06-04 13:08:30 +0200

    completed totalIntegers recursion

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,24 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(list) {
+    let total = 0
+    
+    if (typeof list !== 'object' || list === null) 
+        {
+            return undefined;
+        }
+
+
+    for(item in list) {
+        const value = list[item];
+
+        if (Number.isInteger(value))
+            total += 1;
+
+        else if (typeof value === 'object' && value !== null) {
+            total += totalIntegers(value);
+        }            
+    }
+
+    return total
 };
   
 // Do not edit below this line
