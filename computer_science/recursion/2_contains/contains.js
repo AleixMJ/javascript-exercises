babd8be76654e253c05226cdae97e4a7b1868e5d
AleixMJ <aleixtechIT@gmail.com>
2026-06-04 12:30:20 +0200

    completed contains recursion

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,21 @@
-const contains = function() {
+const contains = function(obj, text) {
+
+    if (Object.values(obj).includes(text)) {
+        return true;
+    }
+
   
+    for (let key in obj) {
+        const value = obj[key]
+
+        if (typeof value === 'object' && value !== null) {
+            if (contains(value, text) === true)
+            return true;
+        }
+    }
+
+    return false;
+
 };
   
 // Do not edit below this line
