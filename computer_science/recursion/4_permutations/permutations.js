e1b1039af07cbfbf551f97f435cc3519470375b9
AleixMJ <aleixtechIT@gmail.com>
2026-06-05 11:49:50 +0200

    comleted permutations recursion

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,18 @@
-const permutations = function() {
+const permutations = function(arr) {
   
+if (arr.length === 0) return [[]];
+
+let result = [];
+
+for (let i = 0; i < arr.length; i++) {
+    const currentNum = arr[i];
+    const subArr = arr.filter((item,index) => index !==i);
+    const newArr = permutations(subArr)
+    const combined = newArr.map(sub => [currentNum, ...sub])
+    result.push(...combined);
+}
+
+return result;
 };
   
 // Do not edit below this line
