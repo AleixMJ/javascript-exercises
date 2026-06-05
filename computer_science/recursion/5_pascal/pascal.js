a2711dee36c550ad10ca03890e00d05b6a99b91f
AleixMJ <aleixtechIT@gmail.com>
2026-06-05 12:14:52 +0200

    completed pascal recursion

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,4 +1,18 @@
-const pascal = function() {
+const pascal = function(n) {
+
+    if (n === 1) {
+        return [1];
+    }
+
+    
+    let prev = pascal(n -1);
+    let processed = prev.map((current, index, arr) => {
+        const next = arr[index + 1];
+        return next !== undefined ? current + next: current;
+    });
+
+    return [1, ...processed];
+
   
 };
   
